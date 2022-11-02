0cf518447c25c45ed599b6af07311158123f7eba
Bahonika <bahonika1@mail.ru>
2022-11-02 20:37:02 +0300

    :fire: Remove code from main

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(),
+    );
   }
 }
